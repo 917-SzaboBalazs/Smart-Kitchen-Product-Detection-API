9e3473aef7d93c582caf5f8cd46687b8d2afe6d1
Szabó Balázs <szabo.balazs2017@gmail.com>
2024-05-24 18:42:25 +0300

    Add fine-tuned resnet model and post request to it

--- api/app/urls.py
+++ api/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import TestView
+from .views import PredictDairyProductView
 
 urlpatterns = [
     path('test/', TestView.as_view(), name='TestView'),
+    path('predict_dairy_product/', PredictDairyProductView.as_view(), name='PredictDairyProductView'),
 ]
--- api/app/views.py
+++ api/app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 
 from rest_framework.response import Response
+import torch
+from PIL import Image
+from torchvision import transforms
+from rest_framework.parsers import MultiPartParser
 
 
 class TestView(APIView):
@@ -15,3 +19,37 @@ class TestView(APIView):
 
         return Response({'message': message})
 
+class PredictDairyProductView(APIView):
+    parser_classes = (MultiPartParser,)
+
+    def __init__(self):
+        super().__init__()
+        self.model = torch.load('model.pth', map_location=torch.device('cpu'))
+        self.model.eval()
+
+    def post(self, request):
+        
+        image_url = request.FILES['image']
+
+        if image_url is None:
+            return Response({'error': 'Image URL is required'}, status=400)
+        
+        # Load image from URL
+        image = Image.open(image_url)
+        preprocess = transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.554, 0.450, 0.343], std=[0.231, 0.241, 0.241]),
+        ])
+
+        image_tensor = preprocess(image)
+        image_tensor = image_tensor.unsqueeze(0)
+
+        # Make prediction
+        with torch.no_grad():
+            prediction = self.model(image_tensor)
+            predicted_label = torch.argmax(prediction, dim=1).item()
+
+        labels = ['butter', 'cheese', 'cottage cheese', 'cream', 'ice cream', 'kefir', 'milk', 'sourcream']
+
+        return Response({'predicted_label': labels[predicted_label]})
